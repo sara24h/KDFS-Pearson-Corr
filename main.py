4520291739502f277d73f22a9308e4a62451b7bb
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-10-07 18:40:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from model.student.MobileNetV2_sparse import MobileNetV2_sparse_deepfake
 from model.student.GoogleNet_sparse import GoogLeNet_sparse_deepfake
 from utils import utils, loss, meter, scheduler
 from train import Train
-from test import Test
+from test_140k import Test
 from finetune import Finetune
 from train_ddp import TrainDDP
 from finetune_ddp import FinetuneDDP
