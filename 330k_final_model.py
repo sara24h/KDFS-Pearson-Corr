b23c3ffc8d1ffcc2a62b97f5e1b46d0e1501c0da
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-10-20 16:00:55 +0330

    Update 330k_final_model.py

--- 330k_final_model.py
+++ 330k_final_model.py
@@ -2,7 +2,7 @@ import torch
 import os
 from model.pruned_model.ResNet_pruned import ResNet_50_pruned_hardfakevsreal
 
-input_save_path = '/kaggle/input/330k-arch-for-fine-tuning/resnet50_pruned_model_learnable_masks.pt'
+input_save_path = '/kaggle/working/resnet50_pruned_model_learnable_masks.pt'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 output_save_path = '/kaggle/working/330k_final.pt'
