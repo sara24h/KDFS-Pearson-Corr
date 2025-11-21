616f0c47ac5973e71b0cb8a1e82255b6d3cb61c1
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-11-21 16:52:19 +0330

    Update dynamic_hesitant.py

--- dynamic_hesitant.py
+++ dynamic_hesitant.py
@@ -549,7 +549,7 @@ def main():
     
     # Models
     MODEL_PATHS = [
-        '/kaggle/input/140k-pearson-pruned/pytorch/default/1/140k_pearson_pruned.pt',
+        '/kaggle/input/140k-finetuned/pytorch/default/1/140k_pearson_ft_best.pt',
         '/kaggle/input/190k-pearson-pruned/pytorch/default/1/190k_pearson_pruned.pt',
         '/kaggle/input/200k-pearson-pruned/pytorch/default/1/200k_kdfs_pruned.pt',
     ]
