00ecf70fc56475d22228b13dd9e334a3534ae3e5
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-12-03 17:09:18 +0330

    Update layer.py

--- model/student/layer.py
+++ model/student/layer.py
@@ -52,8 +52,8 @@ class SoftMaskedConv2d(nn.Module):
 
     def init_mask(self):
         self.mask_weight = nn.Parameter(torch.Tensor(self.out_channels, 2, 1, 1))
-        nn.init.constant_(self.mask_weight[:, 0, :, :], -0.01)   
-        nn.init.constant_(self.mask_weight[:, 1, :, :],  0.01)      
+        nn.init.constant_(self.mask_weight[:, 0, :, :], -0.1)   
+        nn.init.constant_(self.mask_weight[:, 1, :, :],  0.1)      
  
 
     def compute_mask(self, ticket):
