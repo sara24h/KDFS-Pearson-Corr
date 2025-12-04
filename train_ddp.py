792ed5cd38c42412c7388acbc5fe95863dba9051
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-12-04 19:28:35 +0330

    Update train_ddp.py

--- train_ddp.py
+++ train_ddp.py
@@ -39,7 +39,7 @@ class TrainDDP:
         self.warmup_steps = args.warmup_steps
         self.warmup_start_lr = args.warmup_start_lr
         self.lr_decay_T_max = args.lr_decay_T_max
-        self.lr_decay_eta_min = self.lr_decay_eta_min
+        self.lr_decay_eta_min = args.lr_decay_eta_min
         self.weight_decay = args.weight_decay
         self.train_batch_size = args.train_batch_size
         self.eval_batch_size = args.eval_batch_size
